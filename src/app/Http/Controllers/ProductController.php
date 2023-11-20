b86aa1356b4ece6f2646127ba8e7fc7671802d67
林由里恵 <hayashiy@BWNPC050M.local>
2023-11-20 16:57:30 +0900

    商品一覧画面

---
+++ src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Products_image;
+use App\Models\Image;
+use App\Models\Product_inventory_management;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //productsテーブルのproduct_idと一致するproducts_imageテーブルからproduct_idとimage_idを取得します。
+        $products = Product::with(['productImages.image', 'productInventoryManagement'])->get();
+
+
+        return response()->json([
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // $image = $request->file('image');
+        // $path = '';
+        // if (isset($image)) {
+        //     $path = $image->store('images', 'public');
+        // }
+        // return view('images.index', ['path' => $path]);
+
+        // $image = new Image;
+
+        // // name属性が'thumbnail'のinputタグをファイル形式に、画像をpublic/images/productsに保存
+        // $image_path = $request->file('path')->store('public/images/products');
+
+        // // 上記処理にて保存した画像に名前を付け、userテーブルのthumbnailカラムに、格納
+        // $image->path = basename($image_path);
+
+        // $image->save();
+
+        // return redirect()->route('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ src/app/Models/Size_genre.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class size_genre extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    const CREATED_AT = 'signup_at';
+}
